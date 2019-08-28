3b4cbef305aff4702c891e5d99bb572dc35645ee
sunil ravulapalli <sravulapalli@gopfs.com>
2019-08-28 16:37:06 -0600

    Add project files.

---
+++ Builder/ClientApp/src/app/callback/callback.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Component({
+  selector: 'callback',
+  templateUrl: './callback.component.html',
+})
+export class CallbackComponent {
+
+    code: string;
+    clientId: string;
+    constructor(private route: ActivatedRoute, http: HttpClient) {
+
+        this.route.queryParams.subscribe(params => {
+
+            this.clientId = environment.clientId;
+            this.code = params['code'];
+
+
+            var tokenUrl = "https://www.bungie.net/Platform/App/OAuth/Token/";
+
+            const httpOptions = {
+                headers: new HttpHeaders({
+                  'Content-Type': 'application/x-www-form-urlencoded'
+                })
+            };
+
+            var body = "client_id=" + this.clientId + "&grant_type=authorization_code&code=" + this.code;
+            http.post(tokenUrl, body, httpOptions).subscribe(response => {
+              console.log(response);
+            })
+
+        });
+    }
+}
---
+++ Builder/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+@Component({
+  selector: 'app-nav-menu',
+  templateUrl: './nav-menu.component.html',
+  styleUrls: ['./nav-menu.component.css']
+})
+export class NavMenuComponent {
+
+  clientId: string;
+  constructor() {
+    this.clientId = environment.clientId;
+  }
+  isExpanded = false;
+
+  collapse() {
+    this.isExpanded = false;
+  }
+
+  toggle() {
+    this.isExpanded = !this.isExpanded;
+  }
+}
