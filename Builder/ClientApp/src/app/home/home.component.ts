1d0a26b2032bc9b4595eff7d501dbfaa9adfb64e
sunil ravulapalli <rsunilb@hotmail.com>
2019-09-04 21:42:53 -0600

    Multiple items drap drop

--- Builder/ClientApp/src/app/home/home.component.ts
+++ Builder/ClientApp/src/app/home/home.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  equipHelmet = [
+    'Get to work',
+    'Pick up groceries',
+    'Go home',
+    'Fall asleep'
+  ];
 
-  constructor() { }
+  allHelmet = [
+    'Get up',
+    'Brush teeth',
+    'Take a shower',
+    'Check e-mail',
+    'Walk dog'
+  ];
 
-  ngOnInit() {
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
   }
 
 }
